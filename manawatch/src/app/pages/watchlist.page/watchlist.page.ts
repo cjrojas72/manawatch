d089f7840cf36c9c44f78d63fc48800b739fc2b7
cjrojas72 <cjrojas72@gmail.com>
2026-01-22 01:00:37 -0500

    some html cleanup and touching.

--- manawatch/src/app/pages/watchlist.page/watchlist.page.ts
+++ manawatch/src/app/pages/watchlist.page/watchlist.page.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { PriceChartComponent } from '../../components/price-chart/price-chart';
 import { WatchlistList } from '../../components/watchlist-list/watchlist-list';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-watchlist-page',
@@ -9,11 +10,31 @@ import { WatchlistList } from '../../components/watchlist-list/watchlist-list';
   styleUrl: './watchlist.page.css',
 })
 export class WatchlistPage {
+  
+  isAuth = signal(false);
   selectedCardId = signal<string | ''>('');
+  private authService = inject(FirebaseService);
 
+
+
+
+  constructor(){
+     effect(() => {
+      const user = this.authService.currentUser();
+      if (user) {
+        this.isAuth.set(true);
+      } else {
+        this.isAuth.set(false);
+      }
+    });
+  }
 
   logChange(cardId: string | null) {
     console.log('Selected card ID changed to:', cardId);
+  }
+
+  openAuthModal(){
+    this.authService.openLogin();
   }
 
 }
